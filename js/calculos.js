6bb63b77d784a21e7c605f5321f19eefec110b48
German Carosio <carosiogerman@gmail.com>
2023-09-26 00:41:45 -0300

    se agregaron errores, try -catch, async, await. estilos css

--- js/calculos.js
+++ js/calculos.js
@@ -1,7 +1,6 @@
 //https://dolarapi.com/docs#/operations/get-dolar-blue
 
 //variables
-
 const urlBlue = 'https://dolarapi.com/v1/dolares/blue';
 const fecha = document.getElementById('fecha');
 const compra = document.getElementById('compra');
@@ -9,42 +8,67 @@ const venta = document.getElementById('venta');
 const btn = document.getElementById('btn');
 const option = document.getElementById('option');
 
+const verdes = async()=> {
+    //try
+    try {
+        //fetch con await
+        const response = await fetch(urlBlue, {
+            header: 'Accept: application/json'
+        });
 
+        //console.log(response);
 
-fetch(urlBlue, {
-    header: 'Accept: application/json'
-})
-.then(res => res.json())
-.then((data) => {
+        //IFs de STATUS
+        if (response.status === 200) {
+              //acceder a la información
+            const data = await response.json();
 
-    //console.log(data)
+            //console.log(data);
 
-    //Fecha de actualizacion traida de API
-    date = new Date(`${data.fechaActualizacion}`)
-    fecha.innerText = `Fecha de actualizacion = ${date.toLocaleString()}`
+            //Fecha de actualizacion traida de API
+            date = new Date(`${data.fechaActualizacion}`);
+            fecha.innerText = `Actualización = ${date.toLocaleString()}`;
 
-    //Valores Dolar blue traidos de API
-    compra.innerText = `Compra = $ ${data.compra}`
-    venta.innerText = `Venta = $ ${data.venta}`
+            //Valores Dolar blue traidos de API
+            compra.innerText = `Compra = $ ${data.compra}`;
+            venta.innerText = `Venta = $ ${data.venta}`;
 
-    // Accion de boton en Conversor
-    btn.addEventListener('click', ()=>{
+            // Accion de boton en Conversor
+            btn.addEventListener('click', ()=>{
+        
+                let monto = parseFloat(document.getElementById('monto').value);
 
-        let monto = parseFloat(document.getElementById('monto').value);
-        if (monto > 0) {
-            if (option.value == 1) {
-                console.log(option.value);
-                let resultado =  monto/`${data.venta}`
-                document.getElementById('resultado').value = resultado.toFixed(3);
-            } if (option.value == 2) {
-                let resultado = monto*`${data.venta}`
-                document.getElementById('resultado').value = resultado.toFixed(2);
-            }
-             
+                if (monto > 0) {
+                    if (option.value == 1) {
+                        let resultado =  monto/`${data.venta}`;
+                        document.getElementById('resultado').value = resultado.toFixed(2 );
+                    } else if (option.value == 2) {
+                        let resultado = monto*`${data.compra}`;
+                    document.getElementById('resultado').value = resultado.toFixed(2);
+                    }
+                }
+            });
+
+        } else if (response.status === 401) {
+            console.log('Estado 401: No tienes autoridad para acceder al recurso');
+        }  else if (response.status === 403) {
+            console.log('Estado 403: No tienes permisos para acceder');
+        }  else if (response.status === 404) {
+            console.log('Estado 404: Recurso no encontrado');
+        }  else if (response.status === 500) {
+            console.log('Estado 500: Error en el servidor');
+        } else {
+            console.log('Error desconocido');
         }
-    })
-})
+      
+    //catch
+    } catch (error) {
+        console.log(error);
+    }
+};
 
+
+verdes();
 
 
 
